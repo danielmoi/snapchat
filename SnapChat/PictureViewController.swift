6fd9ea779bcb356a7d5b9c8421f62a5e12548854
danielmoi <daniel@ampersandmoi.com>
2017-12-27 14:39:51 +1100

    Set up PictureVC

---
+++ SnapChat/PictureViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PictureViewController.swift
+//  SnapChat
+//
+//  Created by Daniel Moi on 27/12/17.
+//  Copyright © 2017 Daniel Moi. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func nextTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+}
